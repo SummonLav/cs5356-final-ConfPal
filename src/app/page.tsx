805cc1517fe4b3a8186f839db20d6770f6038727
LavendaShan <79836722+SummonLav@users.noreply.github.com>
2025-04-21 17:56:33 -0400

    Solved Clerk to Neon data update problem (hot-loading)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-import { Button } from "@/components/ui/button";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from "@clerk/nextjs";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>homepage content</h1>
